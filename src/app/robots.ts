03e29e7c91eec6c3bc7131365269eca59caa6bf0
dextroussoftech <dexteroussoftech1@gmail.com>
2026-07-17 09:52:35 +0530

    fix: update robots.txt sitemap url to use www domain

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: '/admin',
     },
-    sitemap: 'https://dexteroussoftech.com/sitemap.xml',
+    sitemap: 'https://www.dexteroussoftech.com/sitemap.xml',
   };
 }
