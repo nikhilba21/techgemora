ecd2e340693aad78f2530dafd8e22f7196d53740
dextroussoftech <dexteroussoftech1@gmail.com>
2026-07-24 17:49:42 +0530

    aeo: add llms-full.txt deep knowledge base and grant explicit access to GPTBot,PerplexityBot,ClaudeBot

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,11 +2,17 @@ import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: '/admin',
-    },
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: '/admin',
+      },
+      {
+        userAgent: ['GPTBot', 'PerplexityBot', 'ClaudeBot', 'Google-Extended', 'Applebot-Extended'],
+        allow: '/',
+      }
+    ],
     sitemap: 'https://www.dexteroussoftech.com/sitemap.xml',
   };
 }
